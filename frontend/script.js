669c9278dc402a7c54768d7b55f7a684103591fe
forgacspeter94 <forgacspeter940@gmail.com>
2022-06-23 19:52:12 +0200

    gyak 2

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 console.log('hello world');
 
+let x = 'valtozo vagyok'
 //function loadEvent () {
  //   console.log('loaded');
 
@@ -8,11 +9,67 @@ console.log('hello world');
 
 //window.addEventListener('load',loadEvent)
 
-function loadEvent() {
-    let root = document.getElementById('root');
-    root.insertAdjacentHTML('beforeend',
-    <h1>Tetszőleges mondat</h1>
-    ')
+console.log(x);
 
+x = "En megvaltoztam"
+
+
+const obj = {
+    "kulcs" : "ertek",
+    kulcs2 : 132,
+    boolivan : true,
+    kulcs4 : "123",
+    tomb: ['lara croft','indiana jones'],
+    obivan: {
+        key : 'string',
+        key2 : 951,
+        key3 : ['luke', 'vegtelen']
+            }
 }
+
+const arr = [
+    'string',
+    456,
+    false,
+    '798'
+]
+
+function countryComponent(country) {
+    console.log(country)
+    return
+        <div class = 'country'  >
+            ${countryComponent.name.common}
+        </div>
+}
+
+
+async function loadEvent() {
+    let root = document.getElementById ('root');
+    root.insertAdjacentHTML('beforeend', `
+        <h1>${arr}</h1>
+    ` )
+    for (let index = 0; index <= 10; index++) {
+    console.log('The current value of index is: ',index);
+    root.insertAdjacentHTML('beforeend', `
+    <h1>${index}</h1> 
+    ` )
+    }
+
+    const countries =await fetch('https://restcountries.com/v3.1/all');
+    console.log(countries)
+
+    const countriesJson = await countries.json();
+    console.log(countriesJson);
+    
+    for (let index = 0; index <countriesJson.length; index++) {
+        console.log(countriesJson[index].name.common);
+        root.insertAdjacentHTML('beforeend',countryComponent(countriesJson[index]));
+     
+        }
+}
+
+
+
+
+
 window.addEventListener ('load', loadEvent);
